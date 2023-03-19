7e74efb17072320d8f74f59bed1c905993cadfa2
Marion Terray <mterray@gmail.com>
2023-03-19 22:59:38 +0100

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Descritpion from './Descritpion';
+
+function Banner() {
+  return (
+    <div className='baniere'>
+      <Header />
+      <Descritpion />
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Descritpion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Descritpion() {
+  return (
+    <div className='description'>
+      <p>Les plus belles plantes et fleurs </p>
+    </div>
+  )
+}
+
+export default Descritpion
