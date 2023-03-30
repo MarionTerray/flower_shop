d8001736bb6fe2d8d5f60a8dc4fe4118fa45fd5d
Marion Terray <mterray@gmail.com>
2023-03-30 15:34:39 +0200

    add products

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../styles/ShoppingList.css';
+import { plantList } from '../datas/plantList';
+
+
+function ShoppingList() {
+    const categories = plantList.reduce(
+        (acc, plant) => 
+            acc.includes(plant.category) ? acc : acc.concat(plant.category),
+            []
+    )
+
+  return (
+    <div>
+            <ul>
+                {categories.map((cat) => (
+                    <li key={cat}>{cat}</li>
+                ))}
+            </ul>
+
+            <ul className='plant-list'>
+                {plantList.map((plant) => (
+                    <li key={plant.id} className='plant-item'>
+                    {plant.name}
+                    {plant.isSpecialOffer && <div className='sales'>Soldes</div>}
+                    </li>
+                ))}
+            </ul>
+    </div>
+    
+  )
+}
+
+export default ShoppingList
