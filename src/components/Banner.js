25ca05f7ee707b91c420a47ea93c70d041520f67
Marion Terray <mterray@gmail.com>
2023-03-30 16:05:06 +0200

    final

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import logo from '../assets/logo.png';
 import '../styles/Banner.css'
 
-function Banner() {
-  const title = "Flowers Shop";
-
-  return (
-    <div className='banner'>
-      <img src={logo} alt='Flowers Shop' className='logo' />
-      <h1 className='title'>{title}</h1>
-    </div>
-  )
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
